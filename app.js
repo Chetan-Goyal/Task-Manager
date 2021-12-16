fa1e11a2cea2a3b65d51b6672644b729d5f767d4
Chetan-Goyal <harshrock616@gmail.com>
2021-12-16 22:40:57 +0530

    Basic Http Server

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express();
+
+const port = 3000
+
+
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Server is Listening at http://localhost:${port}`);
+})
