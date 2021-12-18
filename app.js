0de391855d659a0f8f48ee6370106f1f9f3240b5
Chetan-Goyal <harshrock616@gmail.com>
2021-12-18 15:45:43 +0530

    Adds Middlewares for Error Handling

--- app.js
+++ app.js
@@ -6,11 +6,18 @@ const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
 
+const notFound = require('./middlewares/not-found');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
+
 const port = 3000
 
+app.use(express.static('./public'))
 app.use(express.json())
 
 app.use('/api/v1/tasks', tasks)
+
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 const start = async () => {
   try {
