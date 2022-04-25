37517407915eecedf69fa0952f4dbf96ab66a757
Chetan Goyal <harshrock616@gmail.com>
2022-04-25 23:02:29 +0530

    Minor Changes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -15,12 +15,13 @@ const getAllTasks = async (req, res) => {
 const createNewTask = async (req, res) => {
   var taskData = req.body
   taskData.userId = req.userId
-
+  
+  // Modifying Date to correct format
   var _date = new Date(taskData.deadline);
-
   taskData.deadline = _date;
 
   const task = await Task.create(taskData)
+  
   res.status(201).json({ task })
 }
 
